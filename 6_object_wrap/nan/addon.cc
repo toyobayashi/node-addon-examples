6921230cfee6d331eaa3c99f3cd11aa90b7968b1
toyobayashi <lifenglin314@outlook.com>
2022-06-19 16:00:39 +0800

    remove

--- 6_object_wrap/nan/addon.cc
+++
@@ -1,8 +0,0 @@
-#include <nan.h>
-#include "myobject.h"
-
-void InitAll(v8::Local<v8::Object> exports) {
-  MyObject::Init(exports);
-}
-
-NODE_MODULE(addon, InitAll)
--- a-first-project/node-addon-api/lib/binding.js
+++
@@ -1,4 +0,0 @@
-const addon = require('../build/Release/hello-world-native');
-
-module.exports = addon.HelloWorld
-
