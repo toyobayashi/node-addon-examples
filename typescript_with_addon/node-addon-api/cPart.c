1f949a1039ea72edb1ede84262a6c9b89bfee6ba
toyobayashi <lifenglin314@outlook.com>
2022-06-26 00:52:46 +0800

    remove

--- typescript_with_addon/node-addon-api/cPart.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "cPart.h"
-
-int cPart(int value) {
-  int newValue = value + 30;
-  printf("I'm a C function and I received %d from C++ and I'm sending back %d \n",value,newValue);
-  return newValue;
-}
